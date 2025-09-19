9f24848c572138f4f9128a90e5447898a9d02822
asy37 <ahmetsylmz37@gmail.com>
2025-09-19 16:49:13 +0300

    fix: build

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,14 +5,9 @@ import { IBlogPost } from "@/lib/types/blog-post";
 import getQueryClient from "@/lib/utils/getQueryClient";
 import Hydrate from "@/lib/utils/hydrateProvider";
 import { dehydrate } from "@tanstack/react-query";
-import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 export const dynamic = "force-dynamic";
-
-type Props = {
-  params: { slug: string };
-};
 
 const Home = async () => {
   const client = getQueryClient();
@@ -35,25 +30,7 @@ const Home = async () => {
 
 export default Home;
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { slug } = params;
-  const rawData = await apiService.get<IBlogPost[]>(endpoint.blog);
-  const data = rawData.find((item) => item.attributes.slug === slug);
-
-  if (!data?.attributes.seo) {
-    return {
-      title: "Rapkology",
-      description: "Rapkology Haber",
-    };
-  }
-
-  const seo = data.attributes.seo;
-
-  return {
-    title: seo.metaTitle,
-    description: seo.metaDescription,
-    alternates: {
-      canonical: seo.canonicalURL,
-    },
-  };
-}
+export const metadata = {
+  title: "Rapkology",
+  description: "Rapkology Haber",
+};
